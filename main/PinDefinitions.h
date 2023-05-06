079bc28f778c995e2732ab4003ae38f417d45b5c
TomsBicans <tomsbicans@gmail.com>
2023-05-06 15:11:12 +0300

    refactor: Extracted pin definitions outside of main program

---
+++ main/PinDefinitions.h
@@ -0,0 +1,24 @@
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+#include <DHT.h>
+
+#define USER_INPUT_PIN_1 3
+#define USER_INPUT_PIN_2 5
+#define SD_CS_PIN 49
+#define NOISE_PIN 6
+#define DHT_PIN 2
+#define DHT_TYPE DHT22
+#define AIR_QUAL_PIN 8
+#define BUZZER_PIN 9
+#define CO2_PWM_PIN 10
+#define NOISE_SENSOR_A_PIN 0
+
+
+#define UV_INTENSITY_PIN -1
+#define LIGHT_INTENSITY_PIN -1
+#define CO2_AMMOUNT_PIN -1
+#define SMOKE_AMMOUNT_PIN -1
+#define SOUND_PRESSURE_PIN -1
+
+#endif // PIN_DEFINITIONS_H
