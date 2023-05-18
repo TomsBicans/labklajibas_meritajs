295db560897a65ca5ce75674ce4364a99598c112
TomsBicans <tomsbicans@gmail.com>
2023-05-18 13:00:30 +0300

    fix: Improved logger class to save data frequently.

--- main/Logger.cpp
+++ main/Logger.cpp
@@ -1,8 +1,29 @@
 #include "Logger.h"
 
-Logger::Logger(const char* namespaceString) {
-  preferences.begin(namespaceString, false);
+Logger::Logger(const char* namespaceString): namespaceString(namespaceString) {
+  logCount = getLogCount();
+}
+
+void Logger::write_log(LogEntry entry){
+  preferences.begin(this->namespaceString, false);
   logCount = preferences.getUInt(logCountKey, 0);
+  size_t bytesWritten = preferences.putBytes(String(logCount).c_str(), &entry, sizeof(entry));
+  if (bytesWritten == 0) {
+    Serial.println("Failed to write log entry.");
+  } else {
+    Serial.print(bytesWritten);
+    Serial.println(" bytes written for log entry.");
+  }
+
+  size_t op = preferences.putUInt(logCountKey, ++logCount);
+  if (op == 0){
+    Serial.print("Logcount update not successful.");
+  }else{
+    Serial.print(op);
+    Serial.print(" bytes written.");
+    Serial.println("logcount update successful.");
+  }
+  preferences.end();
 }
 
 void Logger::log(uint32_t x_id, uint8_t targetDevice, uint8_t originDevice, SemanticValue semanticValue, float numericValue) {
@@ -15,8 +36,8 @@ void Logger::log(uint32_t x_id, uint8_t targetDevice, uint8_t originDevice, Sema
   entry.numericValue.floatValue = numericValue;
   entry.isFloat = true;
 
-  preferences.putBytes(String(logCount).c_str(), &entry, sizeof(entry));
-  preferences.putUInt(logCountKey, ++logCount);
+  write_log(entry);
+  delay(20);
 }
 
 void Logger::log(uint32_t x_id, uint8_t targetDevice, uint8_t originDevice, SemanticValue semanticValue, long int numericValue) {
@@ -29,24 +50,34 @@ void Logger::log(uint32_t x_id, uint8_t targetDevice, uint8_t originDevice, Sema
   entry.numericValue.intValue = numericValue;
   entry.isFloat = false;
 
-  preferences.putBytes(String(logCount).c_str(), &entry, sizeof(entry));
-  preferences.putUInt(logCountKey, ++logCount);
+  write_log();
+  delay(20);
 }
 
 void Logger::clearLogs() {
+  preferences.begin(this->namespaceString, false);
+  logCount = preferences.getUInt(logCountKey, 0);
   for (uint32_t i = 0; i < logCount; i++) {
     preferences.remove(String(i).c_str());
   }
+  preferences.clear();
   preferences.putUInt(logCountKey, 0);
   logCount = 0;
+  preferences.end();
+  delay(20);
 }
 
 uint32_t Logger::getLogCount() {
+  preferences.begin(namespaceString, true);
+  logCount = preferences.getUInt(logCountKey, 0);
+  preferences.end();
   return logCount;
 }
 
 LogEntry Logger::getLog(uint32_t index) {
   LogEntry entry;
+  preferences.begin(this->namespaceString, true);
   preferences.getBytes(String(index).c_str(), &entry, sizeof(entry));
+  preferences.end();
   return entry;
 }
--- main/Logger.h
+++ main/Logger.h
@@ -4,8 +4,6 @@
 #include <Preferences.h>
 #include <stdio.h>
 
-constexpr const char* LOGGER_NAMESPACE = "logger";
-
 enum SemanticValue : uint8_t {
   // Sensor readings
   SENSOR_HUMIDITY = 1,
@@ -22,7 +20,38 @@ enum SemanticValue : uint8_t {
   // System status messages
   SYSTEM_STATUS_1 = 100,
   SYSTEM_STATUS_2 = 101,
-  SYSTEM_STATUS_3 = 102
+  SYSTEM_STATUS_3 = 102,
+
+  // Device State
+  DEVICE_STATE_FIELD1 = 150,
+  // Transmission Stats
+  TRANSMISSION_STATS_FIELD1 = 155,
+  TRANSMISSION_STATS_FIELD2 = 156,
+  TRANSMISSION_STATS_FIELD3 = 157,
+  TRANSMISSION_STATS_FIELD4 = 158,
+  // Receiver Stats
+  RECEIVER_STATS_FIELD1 = 160,
+  RECEIVER_STATS_FIELD2 = 161,
+  RECEIVER_STATS_FIELD3 = 162,
+  RECEIVER_STATS_FIELD4 = 163,
+  // Device Information
+  DEVICE_INFORMATION_FIELD1 = 165,
+  DEVICE_INFORMATION_FIELD2 = 166,
+
+  // Measurement entry
+  ENTRY_TIME = 170,
+  ENTRY_ATM_TEMPERATURE = 171,
+  ENTRY_ATM_HUMIDITY = 172,
+  ENTRY_ATM_AIR_PRESSURE = 173,
+  ENTRY_ALTITUDE = 174
+  // ENTRY_ATM_AIR_PARTICLE = 175,
+  // ENTRY_ATM_AIR_SMOKE = 176,
+  // ENTRY_ATM_CO2_AMOUNT = 177,
+  // ENTRY_ATM_SOUND_PRESSURE = 178,
+  // ENTRY_LIGHT_INTENSITY = 179,
+  // ENTRY_UV_INTENSITY = 180,
+  // ENTRY_QUALITY_RATING = 181,
+  // ENTRY_USER_LIKES = 182
 };
 
 // Log entry structure
@@ -41,7 +70,7 @@ struct LogEntry {
   bool isFloat;
 
   String toString() const {
-    char buffer[256]; // Adjust the buffer size according to your needs
+    char buffer[512]; // Adjust the buffer size according to your needs
     snprintf(buffer, sizeof(buffer),
              "X_ID: %lu, Target Device: %u, Origin Device: %u, Semantic Value: %u, Timestamp: %lu, Numeric Value: %s, Is Float: %s",
              x_id, targetDevice, originDevice, static_cast<uint8_t>(semanticValue), timestamp,
@@ -51,13 +80,17 @@ struct LogEntry {
   }
 };
 
+constexpr const char* LOGGER_NAMESPACE = "logger";
+constexpr const char* LOGGER_TEST_NAMESPACE = "loggertest";
+
 // Logger class
 class Logger {
   private:
     Preferences preferences;
     uint32_t logCount;
-    const char* logEntryKey = "logentry";
     const char* logCountKey = "logcount";
+    const char* namespaceString;
+    void write_log(LogEntry entry);
 
   public:
     Logger(const char* namespaceString);
