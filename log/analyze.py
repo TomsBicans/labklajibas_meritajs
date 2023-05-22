7b53ea447409d7260f2a35c889c55cf584c717db
TomsBicans <tomsbicans@gmail.com>
2023-05-22 22:40:33 +0300

    feat: 2D graph generator program for log data

--- log/analyze.py
+++ log/analyze.py
@@ -1,51 +1,139 @@
-import sys
-import os
-import serial
-import threading
-import time
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
 import os.path as path
-from pathlib import Path
+import os
+import sys
 
-LOG_DIR = path.dirname(__file__)
-LOG_FILE = path.join(LOG_DIR, "measurement_data.log")
+SemanticValue = {
+    # Sensor readings
+    "SENSOR_HUMIDITY": 1,
+    "SENSOR_TEMPERATURE": 2,
+    "SENSOR_AIR_PRESSURE": 3,
+    "SENSOR_ALTITUDE": 4,
+    # Errors
+    "ERROR_1": 50,
+    "ERROR_2": 51,
+    "ERROR_3": 52,
+    "ERROR_4": 53,
+    "ERROR_5": 54,
+    # System status messages
+    "SYSTEM_STATUS_1": 100,
+    "SYSTEM_STATUS_2": 101,
+    "SYSTEM_STATUS_3": 102,
+    # Device State
+    "DEVICE_STATE_FIELD1": 150,
+    # Transmission Stats
+    "TRANMISSION_PACKETS_SENT": 155,
+    "TRANSMISSION_AVERAGE_SENDING_TIME": 156,
+    "TRANSMISSION_TOTAL_BYTES_SENT": 157,
+    "TRANSMISSION_TOTAL_ON_AIR_TIME": 158,
+    # Receiver Stats
+    "RECEIVER_TOTAL_PACKETS_RECEIVED": 160,
+    "RECEIVER_TOTAL_BYTES_RECEIVED": 161,
+    "RECEIVER_AVERAGE_RSSI": 162,
+    "RECEIVER_AVERAGE_SNR": 163,
+    # Device Information
+    "DEVICE_INFORMATION_FIELD1": 165,
+    "DEVICE_INFORMATION_FIELD2": 166,
+    # Measurement entry
+    "ENTRY_TIME": 170,
+    "ENTRY_ATM_TEMPERATURE": 171,
+    "ENTRY_ATM_HUMIDITY": 172,
+    "ENTRY_ATM_AIR_PRESSURE": 173,
+    "ENTRY_ALTITUDE": 174,
+}
 
 
-def get_data_lines(data: list[str]):
-    res = []
-    for l in data:
-        if any(p.lower() in l.lower() for p in ["OBS", "Total"]):
-            continue
-        res.append(l)
-    return res
+# Function to map a number to the field name
+def number_to_field_name(number):
+    for name, value in SemanticValue.items():
+        if value == number:
+            return name
+    return None  # If no matching field name is found
 
 
-def get_time_lines(data: list[str]):
-    res = []
-    for l in data:
-        if any(p.lower() in l.lower() for p in ["Total"]):
-            res.append(l)
-    return res
+def log_file_graph_loc(logfile: str, semantic_value: int):
+    """Returns the path of the image for a logfile semantic value."""
+    log_dir = os.path.dirname(logfile)
+    logfile_basename = os.path.basename(logfile)
+    logfile_name, _ = os.path.splitext(logfile_basename)
+    plots_dir = os.path.join(log_dir, logfile_name)
+    if not os.path.exists(plots_dir):
+        os.mkdir(plots_dir)
+    plot_loc = os.path.join(plots_dir, f"{number_to_field_name(semantic_value)}.png")
+    return plot_loc
 
 
-def write_to_file(loc: str, data: str):
-    with open(loc, "w") as f:
-        f.write(data)
-    return loc
+def on_key(event):
+    print("event happened")
+    if event.key == " ":
+        plt.close()
 
 
-def main():
-    file = Path(LOG_FILE)
-    data = str(file.read_text())
-    data = data.splitlines()
-    data = [l for l in data if l]
-    time_data = get_time_lines(data)
-    # time_data =
-    data = get_data_lines(data)
-    [print(l) for l in data]
-    data = "\n".join(data) + "\n"
-    loc = path.join(LOG_DIR, "results.csv")
-    write_to_file(loc, data)
+def main(logfile: str):
+    # List to store the log entries
+    log_data = []
+
+    # Open the log file and parse the entries
+    with open(logfile, "r") as file:
+        for line in file:
+            match = re.search(
+                r"Log (\d+): X_ID: (\d+), Target Device: (\d+), Origin Device: (\d+), Semantic Value: (\d+), Timestamp: (\d+), Numeric Value: ([\d\.]+), Is Float: (True|False)",
+                line,
+            )
+            if match:
+                log_data.append(
+                    {
+                        "Log Number": int(match.group(1)),
+                        "X_ID": int(match.group(2)),
+                        "Target Device": int(match.group(3)),
+                        "Origin Device": int(match.group(4)),
+                        "Semantic Value": int(match.group(5)),
+                        "Timestamp": int(match.group(6)) / 1000,  # Convert ms to s
+                        "Numeric Value": float(match.group(7))
+                        if match.group(8) == "True"
+                        else int(match.group(7)),
+                        "Is Float": match.group(8) == "True",
+                    }
+                )
+
+    # Convert the list into a DataFrame
+    df = pd.DataFrame(log_data)
+    # Get unique Semantic Values
+    unique_semantic_values = df["Semantic Value"].unique()
+
+    # Create a figure for each Semantic Value
+    for semantic_value in unique_semantic_values:
+        semantic_df = df[df["Semantic Value"] == semantic_value]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.plot(
+            semantic_df["Timestamp"],
+            semantic_df["Numeric Value"],
+            label=number_to_field_name(semantic_value),
+        )
+        ax.set_title(
+            f"Numeric Value by Timestamp for {number_to_field_name(semantic_value)}"
+        )
+        ax.set_xlabel("Timestamp (seconds)")
+        ax.set_ylabel("Numeric Value")
+        ax.legend()
+        ax.grid(True)  # Add this line to display gridlines
+
+        # Save the figure to a file instead of displaying it
+        fig.savefig(
+            log_file_graph_loc(logfile, semantic_value)
+        )  # Save the plots in a 'plots' directory
+        plt.close(fig)  # Close the figure
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("logfile", help="The log file to be analyzed")
+    args = parser.parse_args()
+    logfile = args.logfile
+    if not path.exists(logfile) and not path.isfile(logfile):
+        print(f"Log file {logfile} does not exist.")
+        sys.exit(1)
+    main(logfile)
